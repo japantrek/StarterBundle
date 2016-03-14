2938f5718567253a727bef127bab96826c0fe30b
nvb <nvb@aproxima.ru>
2016-03-14 10:03:03 +1000

    Route Container - подкласс редиректа, который перенаправляет на первого потомка в PHPCR дереве

---
+++ Admin/RouteContainerAdmin.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace nvbooster\StarterBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\DoctrinePHPCRAdminBundle\Admin\Admin;
+use Symfony\Cmf\Bundle\RoutingBundle\Model\Route;
+
+class RouteContainerAdmin extends Admin
+{
+    protected $translationDomain = 'CmfRoutingBundle';
+
+     /**
+     * Root path for the route parent selection
+     * @var string
+     */
+    protected $routeRoot;
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('path', 'text')
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('form.group_general')
+                ->add('parent', 'doctrine_phpcr_odm_tree', array('choice_list' => array(), 'select_root_node' => true, 'root_node' => $this->routeRoot))
+                ->add('name', 'text')
+            ->end()
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name',  'doctrine_phpcr_nodename')
+        ;
+    }
+
+    public function setRouteRoot($routeRoot)
+    {
+        $this->routeRoot = $routeRoot;
+    }
+
+    public function getExportFormats()
+    {
+        return array();
+    }
+
+    public function toString($object)
+    {
+        return $object instanceof Route && $object->getId()
+            ? $object->getId()
+            : $this->trans('link_add', array(), 'SonataAdminBundle')
+        ;
+    }
+}
---
+++ Document/RouteContainer.php
@@ -0,0 +1,67 @@
+<?php
+namespace nvbooster\StarterBundle\Document;
+
+use Symfony\Cmf\Bundle\RoutingBundle\Doctrine\Phpcr\RedirectRoute;
+use Symfony\Cmf\Component\Routing\RouteObjectInterface;
+
+class RouteContainer extends RedirectRoute
+{
+    /**
+     * {@inheritdoc}
+     * @see \Symfony\Cmf\Component\Routing\RedirectRouteInterface::getRouteName()
+     */
+    public function getRouteName()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @see \Symfony\Cmf\Component\Routing\RedirectRouteInterface::getRouteTarget()
+     */
+    public function getRouteTarget()
+    {
+        return $this->recursiveRouteSearch();
+    }
+
+    /**
+     * {@inheritdoc}
+     * @see \Symfony\Cmf\Component\Routing\RedirectRouteInterface::getUri()
+     */
+    public function getUri()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @see \Symfony\Cmf\Component\Routing\RedirectRouteInterface::isPermanent()
+     */
+    public function isPermanent()
+    {
+        return false;
+    }
+
+    public function recursiveRouteSearch()
+    {
+        foreach ($this->children as $child) {
+            if ($child instanceof RouteContainer) {
+                return $child->getRouteTarget();
+            } elseif ($child instanceof RouteObjectInterface) {
+                return $child;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Get all children of this route including non-routes.
+     *
+     * @return Collection
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+}
